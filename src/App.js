14cea8afb78196b69cbd0b13175a42e291751d56
dongsubak <likeap@catholic.ac.kr>
2020-02-17 12:52:04 +0900

    typo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import logo from './logo.svg';
 import './App.css';
 
 import App1 from './App1';
-//import App2 from './App2';
+import App2 from './App2';
 //import App3 from './App3';
 //import App4 from './App4';
 //import App5 from './App5';
-//import App6 from './App6';
+
 
 class App extends Component {
   render() {
@@ -17,7 +17,7 @@ class App extends Component {
         <div>
           <Route exact path="/" component={App1} />
           <Route exact path="/App1" component={App1}/>
-
+          <Route exact path="/App2" component={App2}/>
         </div>
       </Router>   
     );
